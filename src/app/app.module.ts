af1745b398989429da2bce91987337ecafde3d54
Aditya Yadav <aditya.yadav@dhwaniris.comm>
2024-02-18 11:37:24 +0530

    added component and route structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AirportComponent } from './pages/admin/airport/airport.component';
+import { AllFlightsComponent } from './pages/admin/all-flights/all-flights.component';
+import { BookingsComponent } from './pages/admin/bookings/bookings.component';
+import { CityComponent } from './pages/admin/city/city.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { NewFlightComponent } from './pages/admin/new-flight/new-flight.component';
+import { BookFlightComponent } from './pages/website/book-flight/book-flight.component';
+import { MyBookingsComponent } from './pages/website/my-bookings/my-bookings.component';
+import { SearchComponent } from './pages/website/search/search.component';
+import { WebsiteLandingComponent } from './pages/website/website-landing/website-landing.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AirportComponent,
+    AllFlightsComponent,
+    BookingsComponent,
+    CityComponent,
+    LayoutComponent,
+    LoginComponent,
+    NewFlightComponent,
+    BookFlightComponent,
+    MyBookingsComponent,
+    SearchComponent,
+    WebsiteLandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
